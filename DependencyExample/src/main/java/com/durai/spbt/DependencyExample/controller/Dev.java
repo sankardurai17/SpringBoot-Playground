714393c95538ad6a74ed1fd823e4e13fb0fa1f67
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-13 14:57:05 -0400

    Dependency Injection Methods 2. Setter Injection

--- DependencyExample/src/main/java/com/durai/spbt/DependencyExample/controller/Dev.java
+++ DependencyExample/src/main/java/com/durai/spbt/DependencyExample/controller/Dev.java
@@ -4,6 +4,7 @@ package com.durai.spbt.DependencyExample.controller;
 /*Inorder to make this class eligible for object creation in the IoC container use @component annotation*/
 
 import com.durai.spbt.DependencyExample.service.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,15 @@ public class Dev {
     //3 ways of Dependency Injection
 
     /*1. Constructor Injection*/
-    public Dev(Laptop laptop) {
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+    /*2. Setter Injection using @autowired
+    Auto wire is used to connect Dev to Laptop class basically it connects the reference
+    * */
+    @Autowired
+    public void setLaptop(Laptop laptop) {
         this.laptop = laptop;
     }
 
