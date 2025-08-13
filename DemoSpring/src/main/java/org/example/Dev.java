4b0fab37c54cc7e6d0c42785bf1a09cfec3cb9f8
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-13 17:15:54 -0400

    Injecting dependency using constructor

--- DemoSpring/src/main/java/org/example/Dev.java
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class Dev {
+    /*To use the Laptop functionalities we need to create a bean for Laptop in spring config .xml and inject the dependency
+    to Dev class. It can be done as usual in three ways constructor, setter and autowire by name / by type*/
+    private Laptop laptop;
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+    /*1. Dependency Inject by Constructor*/
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     public void code(){
+        laptop.compile();
         System.out.println("Developer is coding");
     }
 }
