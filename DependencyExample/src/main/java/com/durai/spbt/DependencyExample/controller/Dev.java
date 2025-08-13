d847404a6a8868efd11bf709a11a5fb605f932db
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-13 14:51:34 -0400

    Dependency Injection Methods 1. Constructor Injection

--- DependencyExample/src/main/java/com/durai/spbt/DependencyExample/controller/Dev.java
+++ DependencyExample/src/main/java/com/durai/spbt/DependencyExample/controller/Dev.java
@@ -3,14 +3,23 @@ package com.durai.spbt.DependencyExample.controller;
 
 /*Inorder to make this class eligible for object creation in the IoC container use @component annotation*/
 
+import com.durai.spbt.DependencyExample.service.Laptop;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    private Laptop laptop;
+    //3 ways of Dependency Injection
+
+    /*1. Constructor Injection*/
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+    }
 
 
     public void code(){
     System.out.println("Developer is coding");
+        laptop.compile();
     }
 
 }
---
+++ DependencyExample/src/main/java/com/durai/spbt/DependencyExample/service/Laptop.java
@@ -0,0 +1,11 @@
+package com.durai.spbt.DependencyExample.service;
+
+import org.springframework.stereotype.Component;
+
+/*Inorder to make this class eligible for object creation in the IoC container use @component annotation*/
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling in Laptop");
+    }
+}
