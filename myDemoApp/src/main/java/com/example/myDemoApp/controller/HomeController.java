06e4327b9b08fba3b68e4f35ddbc1923fcf6792b
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-14 17:04:43 -0400

    Basic Controller

---
+++ myDemoApp/src/main/java/com/example/myDemoApp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.myDemoApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/greet")
+    public String greet(){
+        return "Welcome to My Demo App";
+    }
+}
