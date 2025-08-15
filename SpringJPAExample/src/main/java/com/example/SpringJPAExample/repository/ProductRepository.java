69c0e41b32ae4452ab36566b52b0c0d38a6fcba6
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-14 21:31:39 -0400

    comments

--- SpringJPAExample/src/main/java/com/example/SpringJPAExample/repository/ProductRepository.java
+++ SpringJPAExample/src/main/java/com/example/SpringJPAExample/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.example.SpringJPAExample.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Repository interface should extends JpaRepository class
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
