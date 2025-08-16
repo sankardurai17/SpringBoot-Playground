6fb1835a876f8a6b511048912ea8947c35cffc81
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-08-16 14:59:55 -0400

    Printing session ID along with the message

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/controller/HelloController.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecurityDemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public ResponseEntity<?> greeting(){
-        return ResponseEntity.ok("Welcome to My Spring Security Demo");
+    public ResponseEntity<?> greeting(HttpServletRequest request) {
+        return ResponseEntity.ok("Welcome to My Spring Security Demo " + request.getSession().getId());
     }
+
+
 }
